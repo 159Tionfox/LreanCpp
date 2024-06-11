e3e9f8458b7aeea1a08d3516a517d4b2a124e39c
神煜森 <h18552897585@outlook.com>
2024-06-11 15:21:50 +0800

    创建Using.h ，Main.cpp。

---
+++ Main.cpp
@@ -0,0 +1,6 @@
+//主体结构，只要是CMD程序那么必须含有一下代码片段
+#include "Using.h"
+
+int main() {
+	return 0;
+}
---
+++ Using.h
@@ -0,0 +1,8 @@
+//使用的头文件放在此次
+
+//A阶段仅需以下头文件
+#include <iostream>
+
+//B阶段需包含A且有以下头文件
+#include <vector>
+#include <map>
